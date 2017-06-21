094e3398df93ceb5d43c765c3344176cf24b96cc
epli2 <motoryo1@gmail.com>
2017-06-21 20:56:28 +0900

    use delegating constructors

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -1,19 +1,6 @@
 #include "Enemy.h"
 
-Enemy::Enemy()
-  : position_(ofVec3f(0, 0, 0)), hp_(100), attack_(50), color_(RED) {
-  body_.loadModel(modelpath_body_);
-  light_.loadModel(modelpath_light_);
-  body_.setScale(0.5, 0.5, 0.5);
-  light_.setScale(0.5, 0.5, 0.5);
-  body_.setRotation(0, 180, 1, 0, 0);
-  light_.setRotation(0, 180, 1, 0, 0);
-  body_.enableMaterials();
-  for (int i = 0; i < NUM; i++) {
-    freq[i] = ofRandom(4.0, 10.0);
-  }
-  shader.load("", "shader/shader_sin.frag");
-}
+Enemy::Enemy() : Enemy(ofVec3f(0, 0, 0), 100, 50, RED) {}
 
 Enemy::Enemy(ofVec3f _position, float _hp, float _attack, Color _color)
     : position_(_position), hp_(_hp), attack_(_attack), color_(_color) {
