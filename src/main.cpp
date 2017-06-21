b37672eba2677c7599038952cdb80aaee1197f7e
epli2 <motoryo1@gmail.com>
2017-06-22 02:46:05 +0900

    remove unnecessary comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,13 @@
 #include "ofApp.h"
 #include "ofMain.h"
 
-//========================================================================
 int main() {
 #ifdef USE_DIMENCO_OPENGL_INTERFACE
   ofAppGLFWWindow win;
   win.setMultiDisplayFullscreen(true);
   ofSetupOpenGL(&win, 640, 480, OF_FULLSCREEN);
 #else
-  ofSetupOpenGL(1600, 900, OF_WINDOW); // <-------- setup the GL context
+  ofSetupOpenGL(1600, 900, OF_WINDOW);
 #endif
-  // this kicks off the running of my app
-  // can be OF_WINDOW or OF_FULLSCREEN
-  // pass in width and height too:
   ofRunApp(new ofApp());
 }
