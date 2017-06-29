31341a0aada42391be335f9b665b5ebc0a3cb0ad
epli2 <motoryo1@gmail.com>
2017-06-29 17:44:33 +0900

    インデントを修正

--- src/Bullet.h
+++ src/Bullet.h
@@ -6,19 +6,19 @@
 #include "ColliderObject.h"
 
 class Bullet : public ColliderObject {
-  private:
-    float lifetime_;
-    float lifecount_;
-    float radius_;
-    ofVec3f acceleration_;
-    ofVec3f velocity_;
-    ofVboMesh mesh_;
-    std::vector<Particle> particles;
+private:
+  float lifetime_;
+  float lifecount_;
+  float radius_;
+  ofVec3f acceleration_;
+  ofVec3f velocity_;
+  ofVboMesh mesh_;
+  std::vector<Particle> particles;
 
-  public:
-    Bullet(ofVec3f _position, float _radius, Color _color);
-    void Update();
-    void Draw();
-    bool IsDie() const;
-    ~Bullet();
+public:
+  Bullet(ofVec3f _position, float _radius, Color _color);
+  void Update();
+  void Draw();
+  bool IsDie() const;
+  ~Bullet();
 };
