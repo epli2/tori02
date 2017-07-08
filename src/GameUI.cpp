76dbb5394506e73377c50260b566c0b7909071e7
epli2 <motoryo1@gmail.com>
2017-07-08 22:29:11 +0900

    Update GameUI

--- src/GameUI.cpp
+++ src/GameUI.cpp
@@ -23,6 +23,25 @@ void GameUI::Setup() {
   verdana.load("Verdana.ttf", 30);
   verdana.setLineHeight(24);
   verdana.setLetterSpacing(1.0);
+
+  uipanel_.loadModel(modelpath_uipanel_);
+  uimat_.loadModel(modelpath_uimat_);
+  uibar_r_.loadModel(modelpath_uibar_[0]);
+  uibar_g_.loadModel(modelpath_uibar_[1]);
+  uibar_b_.loadModel(modelpath_uibar_[2]);
+  uipanel_.setScaleNormalization(false);
+  uimat_.setScaleNormalization(false);
+  uibar_r_.setScaleNormalization(false);
+  uibar_g_.setScaleNormalization(false);
+  uibar_b_.setScaleNormalization(false);
+  uipanel_.setScale(20, 20, 1);
+  uimat_.setScale(20, 20, 10);
+  uibar_r_.setScale(20, 20, 10);
+  uibar_g_.setScale(20, 20, 10);
+  uibar_b_.setScale(20, 20, 10);
+  //uipanel_.enableMaterials();
+  uimat_.enableMaterials();
+  shader_.load("", shaderpath_uimat_);
 }
 
 void GameUI::Update() {}
@@ -30,14 +49,33 @@ void GameUI::Update() {}
 void GameUI::Draw() {
 
   // ofSetColor(255, 255, 255);
-  ofSetColor(ccolor_);
-  ofCircle(100, ofGetHeight() - 100, 50);
+  // ofSetColor(ccolor_);
+  // ofCircle(100, ofGetHeight() - 100, 50);
 
   char sc[255];
   sprintf(sc, "SCORE : %d", sp_);
   ofSetColor(30, 200, 200);
-  verdana.drawString(sc, 750, ofGetHeight() / 20);
+  verdana.drawString(sc, ofGetWidth() - (ofGetWidth() / 9), ofGetHeight() / 20);
 
+  uipanel_.setPosition(0, ofGetHeight(), 0);
+  uipanel_.drawFaces();
+  uimat_.setPosition(0, ofGetHeight(), 0);
+  shader_.begin();
+  shader_.setUniform1f("time", ofGetElapsedTimef());
+  shader_.setUniform2f("resolution", ofGetWidth(), ofGetHeight());
+  shader_.setUniform3f("color", ccolor_.r / 255.0, ccolor_.g / 255.0, ccolor_.b / 255.0);
+  uimat_.drawFaces();
+
+  uibar_r_.setPosition(0, ofGetHeight(), 0);
+  uibar_g_.setPosition(0, ofGetHeight(), 0);
+  uibar_b_.setPosition(0, ofGetHeight(), 0);
+  shader_.setUniform3f("color", 232.0 / 255.0, 60.0 / 255.0, 55.0 / 255.0);
+  uibar_r_.drawFaces();
+  shader_.setUniform3f("color", 72.0 / 255.0, 232.0 / 255.0, 124.0 / 255.0);
+  uibar_g_.drawFaces();
+  shader_.setUniform3f("color", 53.0 / 255.0, 61.0 / 255.0, 255.0 / 255.0);
+  uibar_b_.drawFaces();
+  shader_.end();
   // gui1.draw();
 }
 
--- src/GameUI.h
+++ src/GameUI.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxAssimpModelLoader.h"
 #include "Common.h"
 // #include "ofxGui.h"
 
@@ -11,6 +12,18 @@ private:
   Color color_;
   ofColor ccolor_;
   ofTrueTypeFont verdana;
+  std::string modelpath_uipanel_ = "models2/UI1.obj";
+  std::string modelpath_uimat_ = "models2/UI1_light.obj";
+  array<std::string, 3> modelpath_uibar_{ "models2/UI1_red.obj",
+                                          "models2/UI1_green.obj",
+                                          "models2/UI1_blue.obj" };
+  std::string shaderpath_uimat_ = "shader/shader_ball.frag";
+  ofxAssimpModelLoader uipanel_;
+  ofxAssimpModelLoader uimat_;
+  ofxAssimpModelLoader uibar_r_;
+  ofxAssimpModelLoader uibar_g_;
+  ofxAssimpModelLoader uibar_b_;
+  ofShader shader_;
 
 public:
   GameUI();
